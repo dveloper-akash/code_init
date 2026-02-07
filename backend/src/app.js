139ed371a6a7feb4f94b0ae6197f1a424f42d5bd
Akash <developerakash99@gmail.com>
2026-02-07 12:53:17 +0530

    feat: updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,28 @@
 import express from "express";
 import routes from "./route.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors())
+app.get("/",(req,res)=>{
+  res.send("GridX backend is running!!!")
+})
 
 app.use("/api", routes);
 
-
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Grid-X backend running on http://localhost:${PORT}`);
 });
 
 export default app;
